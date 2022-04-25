90096bad5b2bf20cf2ffe7340ea73cb2a32be983
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-04-25 22:56:17 +0100

    first tensor flow project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,87 @@
-import logo from './logo.svg';
+import React, {useRef} from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
 import './App.css';
+import {drawHand} from "./utils";
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runHandpose = async () =>{
+    const net = await handpose.load()
+    console.log("Handpose model loaded");
+    //loop and detect hands
+    setInterval(() =>{
+      detect(net)
+    },100)
+
+  };
+
+  const detect = async (net) => {
+    // check data is available
+    if(
+      typeof webcamRef.current !== "undefined" && 
+      webcamRef.current !== "null" && 
+      webcamRef.current.video.readyState === 4
+    ) {
+      //get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      //set video height and wdith
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      //set canvasheight and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      //make detetcion
+      const hand = await net.estimateHands(video);
+      console.log(hand); 
+
+      //draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
+    }
+  };
+
+  runHandpose();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Webcam 
+            ref ={webcamRef}
+            style={{
+              position:"absolute",
+              marginLeft:"auto",
+              marginRight:"auto",
+              left:0,
+              right:0,
+              textAlign:"center",
+              zindex:9,
+              width:640,
+              height:480
+            }}
+          />
+          <canvas 
+            ref ={canvasRef}
+            style={{
+              position:"absolute",
+              marginLeft:"auto",
+              marginRight:"auto",
+              left:0,
+              right:0,
+              textAlign:"center",
+              zindex:9,
+              width:640,
+              height:480
+            }} 
+          />
       </header>
     </div>
   );
---
+++ src/utils.js
@@ -0,0 +1,70 @@
+
+//pointf for fingers
+const fingerJoints = {
+    thumb:[0, 1, 2, 3, 4],
+    indexFinger:[0, 5, 6, 7, 8],
+    middleFinger:[0, 9, 10, 11, 12],
+    ringFinger:[0, 13, 14, 15, 16],
+    pinky:[0, 17, 18, 19, 20],
+};
+
+
+
+
+
+// drawing funciton 
+export const drawHand = (predictions, ctx) => {
+    //check if we have predictions
+    if(predictions.length>0){
+        //loop through each prediction
+        predictions.forEach((predictions)=>{
+
+
+
+            //grab landmarks
+            const landmarks = predictions.landmarks;
+
+            //loop through fingers
+            for(let j = 0; j <Object.keys(fingerJoints).length; j++){
+                let finger = Object.keys(fingerJoints)[j];
+                //loop through pairs of joints
+                for(let k=0; k<fingerJoints[finger].length -1; k++){
+                    const firstJointIndex = fingerJoints[finger][k];
+                    const secondJointIndex = fingerJoints[finger][k+1];
+
+                    // draw path
+                    ctx.beginPath();
+                    ctx.moveTo(
+                        landmarks[firstJointIndex][0],
+                        landmarks[firstJointIndex][1],
+                    );
+                    ctx.lineTo(
+                        landmarks[secondJointIndex][0],
+                        landmarks[secondJointIndex][1],
+                    )
+                    ctx.strokeStyle = "plum";
+                    ctx.lineWidth = 4;
+                    ctx.stroke();
+                }
+            }
+
+
+
+
+            //loop through the landmarkjs and draw
+            for (let i =0; i<landmarks.length; i++){
+                //get x point
+                const x = landmarks[i][0];
+                //get y point
+                const y = landmarks[i][1];
+                //start drawing
+                ctx.beginPath();
+                ctx.arc(x, y, 5, 0, 3 * Math.PI);
+                //set line color
+                ctx.fillStyle = "aqua";
+                ctx.fill();
+
+            }
+        })
+    }
+}
